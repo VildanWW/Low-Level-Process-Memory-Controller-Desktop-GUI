b37fbf05ebc8ed8b67512281e9526c7a4fe6875a
VildanWW <akbulatov.vildan@gmail.com>
2026-09-16 00:54:06 +0500

    Update

---
+++ Core/Core/BaseFunctional.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+class BaseFunctional {
+public:
+	virtual void Initialize() = 0;
+	virtual void ShutDown() = 0;
+
+	virtual ~BaseFunctional() = default;
+};
+
--- Core/Core/dllmain.cpp
+++ Core/Core/dllmain.cpp
@@ -1,13 +1,37 @@
 ﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
 #include "pch.h"
+#include "MinHook.h"
+#include "BaseFunctional.h"
+#include <memory>
+#include <vector>
+#include <condition_variable>
+
+DWORD WINAPI MainThread(LPVOID lpParam) {
+    MH_Initialize();
+    std::condition_variable cvMainThread;
+
+    std::vector<std::unique_ptr<BaseFunctional>> functional;
+
+    for (int i = 0; i < functional.size(); i++) {
+        functional[i]->Initialize();
+    }
+
+    while (!(GetAsyncKeyState(VK_END) & 0x8000)) {
+        Sleep(100);
+    }
+
+    for (int i = 0; i < functional.size(); i++) {
+        functional[i]->ShutDown();
+    }
+
+    MH_Uninitialize();
+    FreeLibraryAndExitThread((HMODULE)lpParam, 0);
+    return 0;
+}
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+        CreateThread(NULL, 0, MainThread, hModule, 0, NULL);
     }
     return TRUE;
 }
