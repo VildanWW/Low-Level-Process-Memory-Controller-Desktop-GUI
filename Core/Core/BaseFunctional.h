703582f57a1a11006238ba5fcca416683dbef18b
VildanWW <akbulatov.vildan@gmail.com>
2026-09-16 23:55:35 +0500

    Added basic MovementController

--- Core/Core/BaseFunctional.h
+++ Core/Core/BaseFunctional.h
@@ -1,4 +1,6 @@
 ﻿#pragma once
+#include "BaseFunctional.h"
+
 class BaseFunctional {
 public:
 	virtual void Initialize() = 0;
---
+++ Core/Core/Offsets.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <cstdint>
+
+struct Offsets {
+	static constexpr uintptr_t jumpAddress = 0x1E94C0;
+
+	static inline uintptr_t clientBase = 0;
+};
+
+struct GameFunctions {
+	static constexpr uintptr_t moveAddress = 0x9B880;
+};
